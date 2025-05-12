fa50e65b0f9305bdc223e545ae9f0cd0c83b289f
Sriram <153843+yesoreyeram@users.noreply.github.com>
2025-05-12 11:32:46 +0100

    added bench tests for ApplyRootSelector in jsonframer (#22)
    * added bench tests for ApplyRootSelector in jsonframer
    
    * Apply suggestions from code review
    
    Co-authored-by: ismail simsek <ismailsimsek09@gmail.com>
    
    ---------
    
    Co-authored-by: ismail simsek <ismailsimsek09@gmail.com>

--- lib/go/jsonframer/jsonframer.go
+++ lib/go/jsonframer/jsonframer.go
@@ -8,9 +8,7 @@ import (
 
 	"github.com/grafana/grafana-plugin-sdk-go/data"
 	"github.com/grafana/infinity-libs/lib/go/gframer"
-	"github.com/itchyny/gojq"
 	"github.com/tidwall/gjson"
-	jsonata "github.com/xiatechs/jsonata-go"
 )
 
 type FramerType string
@@ -61,7 +59,7 @@ func ToFrames(jsonString string, options FramerOptions) (frames []*data.Frame, e
 		return frames, err
 	}
 
-	outString, err := GetRootData(jsonString, options.RootSelector, options.FramerType)
+	outString, err := ApplyRootSelector(jsonString, options.RootSelector, options.FramerType)
 	if err != nil {
 		return frames, err
 	}
@@ -142,7 +140,7 @@ func ToFrame(jsonString string, options FramerOptions) (frame *data.Frame, err e
 	if err != nil {
 		return frame, err
 	}
-	outString, err := GetRootData(jsonString, options.RootSelector, options.FramerType)
+	outString, err := ApplyRootSelector(jsonString, options.RootSelector, options.FramerType)
 	if err != nil {
 		return frame, err
 	}
@@ -151,79 +149,6 @@ func ToFrame(jsonString string, options FramerOptions) (frame *data.Frame, err e
 		return frame, err
 	}
 	return getFrameFromResponseString(outString, options)
-}
-
-func GetRootData(jsonString string, rootSelector string, framerType FramerType) (string, error) {
-	if rootSelector == "" {
-		return jsonString, nil
-	}
-	if framerType == FramerTypeJQ {
-		query, err := gojq.Parse(rootSelector)
-		if err != nil {
-			return "", errors.Join(ErrInvalidJQSelector, err)
-		}
-		var data any
-		err = json.Unmarshal([]byte(jsonString), &data)
-		if err != nil {
-			return "", errors.Join(ErrUnMarshalingJSON, err)
-		}
-		iter := query.Run(data)
-		out := []any{}
-		for {
-			v, ok := iter.Next()
-			if !ok {
-				break
-			}
-			if err, ok := v.(error); ok {
-				if err, ok := err.(*gojq.HaltError); ok && err.Value() == nil {
-					break
-				}
-				return "", errors.Join(ErrExecutingJQ, err)
-			}
-			out = append(out, v)
-		}
-		// if the result is array of 1 element array, ignore the outer array and return that single element from inner array
-		if len(out) == 1 {
-			if v, ok := out[0].([]any); ok {
-				outStr, err := json.Marshal(v)
-				if err != nil {
-					return "", errors.Join(ErrMarshalingJSON, err)
-				}
-				return string(outStr), nil
-			}
-		}
-		outStr, err := json.Marshal(out)
-		if err != nil {
-			return "", errors.Join(ErrMarshalingJSON, err)
-		}
-		return string(outStr), nil
-	}
-
-	r := gjson.Get(string(jsonString), rootSelector)
-	if r.Exists() {
-		return r.String(), nil
-	}
-	expr, err := jsonata.Compile(rootSelector)
-	if err != nil {
-		return "", errors.Join(ErrInvalidRootSelector, err)
-	}
-	if expr == nil {
-		return "", errors.Join(ErrInvalidRootSelector)
-	}
-	var data any
-	err = json.Unmarshal([]byte(jsonString), &data)
-	if err != nil {
-		return "", errors.Join(ErrInvalidJSONContent, err)
-	}
-	res, err := expr.Eval(data)
-	if err != nil {
-		return "", errors.Join(ErrEvaluatingJSONata, err)
-	}
-	r2, err := json.Marshal(res)
-	if err != nil {
-		return "", errors.Join(ErrInvalidJSONContent, err)
-	}
-	return string(r2), nil
 }
 
 func getColumnValuesFromResponseString(responseString string, columns []ColumnSelector) (string, error) {
